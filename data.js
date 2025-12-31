38834e4d5f9ee83bf12c474f4636ef0ef3eb5c8d
GreenBaboon <65962651+GreenBaboons@users.noreply.github.com>
2025-12-31 10:22:47 +0100

    Update data.js

--- data.js
+++ data.js
@@ -26,5 +26,131 @@ const quizData = [
         "Vraag": "",
         "Antwoord": "Liever te dik in de kist. Dan weer een feestje gemist.",
         "Info": ""
+    },
+    {
+        "Type": "Song & Artist",
+        "URL": "https://www.youtube.com/watch?v=idBsmslkKDs&list=RDidBsmslkKDs&start_radio=1",
+        "StartTijd": 30,
+        "EindTijd": 90,
+        "Vraag": "Who's song is this?",
+        "Antwoord": "Queen - Bohemian Rhapsody",
+        "Info": "Uit 1975"
+    },
+    {
+        "Type": "Maak de zin af",
+        "URL": "https://www.youtube.com/watch?v=eg8YHJmmLfc&list=RDeg8YHJmmLfc&start_radio=1",
+        "StartTijd": 0,
+        "EindTijd": 40,
+        "Vraag": "",
+        "Antwoord": "Liever te dik in de kist. Dan weer een feestje gemist.",
+        "Info": ""
+    },
+    {
+        "Type": "Mashup",
+        "URL": "https://www.youtube.com/watch?v=_AGYdfip4x0&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Heathens- Twenty One Pilots Heathens (instrumental)- Twenty One Pilots Miss Jackson- Panic! at the disco Soap (acapella)- Melanie Martinez Do I wanna know? (acapella)- Arctic Monkeys I Write sins not tragedies (acapella)- Panic! at the disco Stressed Out (acapella) by Twenty One Pilots",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=gpXVbP5Q_uA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=3",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Hey Soul Sister & Fireflies",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=JlA06jrtY2s&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=4",
+        "StartTijd": 11,
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Adele Feat. Eminem Skyfall/Lose yourself Mash",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=NN75im_us4k&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=5",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Nirvana - Smells Like Teen Spirit & Rick Astley - Never Gonna Give You Up",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=EJEZ_HpmdvA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=8",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Britney Spears - Toxic & Korn - Twisted Transistor",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=obBelA1srtw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=9",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Disturbed - Down With The Sickness Taylor Swift - Blank Space",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=0MjysDl_nGw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=11",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Michael Bublé vs. Fall Out Boy",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=FaZ5c48bA2E&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=12",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Wake Me Up When September Ends by Green Day vs. Black Hole Sun by Soundgarden",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=QmahTV5LOs4&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=13",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Shape of You by Ed Sheeran vs. Fireflies by Owl City",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=IjWJK47ssRI&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=14",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Photograph by Ed Sheeran vs. Helena by My Chemical Romance",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=DhvXST1Rc3g&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=15",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Taylor Swift - Shake It Off Nine Inch Nails - The Perfect Drug",
+        "Info": ""
+    },
+    {
+        "Type": "Guess the songs",
+        "URL": "https://www.youtube.com/watch?v=yO3MwSbsju8&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=17",
+        "StartTijd": "",
+        "EindTijd": "",
+        "Vraag": "What songs do you hear?",
+        "Antwoord": "Rammstein vs Lady Gaga - Du Hast vs Telephone",
+        "Info": ""
     }
 ]
