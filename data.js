74bffb86dd0930965a85823fea5884820faf6596
GreenBaboon <65962651+GreenBaboons@users.noreply.github.com>
2025-12-31 11:36:24 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,114 +1,286 @@
 // data.js
 const quizData = [
-    {
-        "Type": "Mashup",
-        "URL": "https://www.youtube.com/watch?v=_AGYdfip4x0&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Heathens- Twenty One Pilots Heathens (instrumental)- Twenty One Pilots Miss Jackson- Panic! at the disco Soap (acapella)- Melanie Martinez Do I wanna know? (acapella)- Arctic Monkeys I Write sins not tragedies (acapella)- Panic! at the disco Stressed Out (acapella) by Twenty One Pilots",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=gpXVbP5Q_uA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=3",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Hey Soul Sister & Fireflies",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=JlA06jrtY2s&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=4",
-        "StartTijd": 11,
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Adele Feat. Eminem Skyfall/Lose yourself Mash",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=NN75im_us4k&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=5",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Nirvana - Smells Like Teen Spirit & Rick Astley - Never Gonna Give You Up",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=EJEZ_HpmdvA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=8",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Britney Spears - Toxic & Korn - Twisted Transistor",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=obBelA1srtw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=9",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Disturbed - Down With The Sickness Taylor Swift - Blank Space",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=0MjysDl_nGw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=11",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Michael Bublé vs. Fall Out Boy",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=FaZ5c48bA2E&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=12",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Wake Me Up When September Ends by Green Day vs. Black Hole Sun by Soundgarden",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=QmahTV5LOs4&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=13",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Shape of You by Ed Sheeran vs. Fireflies by Owl City",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=IjWJK47ssRI&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=14",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Photograph by Ed Sheeran vs. Helena by My Chemical Romance",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=DhvXST1Rc3g&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=15",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Taylor Swift - Shake It Off Nine Inch Nails - The Perfect Drug",
-        "Info": ""
-    },
-    {
-        "Type": "Guess the songs",
-        "URL": "https://www.youtube.com/watch?v=yO3MwSbsju8&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=17",
-        "StartTijd": "",
-        "EindTijd": "",
-        "Vraag": "What songs do you hear?",
-        "Antwoord": "Rammstein vs Lady Gaga - Du Hast vs Telephone",
-        "Info": ""
-    }
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=_AGYdfip4x0&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Heathens- Twenty One Pilots Heathens (instrumental)- Twenty One Pilots Miss Jackson- Panic! at the disco Soap (acapella)- Melanie Martinez Do I wanna know? (acapella)- Arctic Monkeys I Write sins not tragedies (acapella)- Panic! at the disco Stressed Out (acapella) by Twenty One Pilots",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=gpXVbP5Q_uA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=3",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Hey Soul Sister & Fireflies",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=JlA06jrtY2s&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=4",
+    "StartTijd": 11,
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Adele Feat. Eminem Skyfall/Lose yourself Mash",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=NN75im_us4k&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=5",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Nirvana - Smells Like Teen Spirit & Rick Astley - Never Gonna Give You Up",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=EJEZ_HpmdvA&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=8",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Britney Spears - Toxic & Korn - Twisted Transistor",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=obBelA1srtw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=9",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Disturbed - Down With The Sickness Taylor Swift - Blank Space",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=0MjysDl_nGw&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=11",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Michael Bublé vs. Fall Out Boy",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Song & Artist",
+    "URL": "https://www.youtube.com/watch?v=idBsmslkKDs&list=RDidBsmslkKDs&start_radio=1",
+    "StartTijd": 30,
+    "EindTijd": 90,
+    "Vraag": "Who's song is this?",
+    "Antwoord": "Queen - Bohemian Rhapsody",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=FaZ5c48bA2E&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=12",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Wake Me Up When September Ends by Green Day vs. Black Hole Sun by Soundgarden",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=QmahTV5LOs4&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=13",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Shape of You by Ed Sheeran vs. Fireflies by Owl City",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=IjWJK47ssRI&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=14",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Photograph by Ed Sheeran vs. Helena by My Chemical Romance",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the artist + song",
+    "URL": "https://www.youtube.com/watch?v=nkviJlvlzjI&list=RDnkviJlvlzjI&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "Who's the original artist?",
+    "Antwoord": "Limp Bizkit - Rollin'",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=DhvXST1Rc3g&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=15",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Taylor Swift - Shake It Off Nine Inch Nails - The Perfect Drug",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=yO3MwSbsju8&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=17",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Rammstein vs Lady Gaga - Du Hast vs Telephone",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=F-7oB-tXZ7c&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=19",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Marilyn Manson's 'Sweet Dreams (Are Made Of This)' and Demi Lovato's 'Cool For The Summer'",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the artist + song",
+    "URL": "https://www.youtube.com/watch?v=U6g6PkMlbSU&list=RDU6g6PkMlbSU&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Rammstein - Deutschland",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=WC1ei5RlK7E&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=20",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Head Like A Hole - Nine Inch Nails vs. I Really Like You - Carly Rae Jepsen",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=Ocj2Le9IZ-I&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR&index=23",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Meghan Trainor vs. Katy Perry",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=oT3mCybbhf0&list=RDoT3mCybbhf0&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "AVICII & RICK ASTLEY - Never Gonna Give You Up & Wake Me Up",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=wTFMWfvMoh0&list=RDwTFMWfvMoh0&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Backstreet Boys - I Want it That",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=vVldzcsOprk&list=RDvVldzcsOprk&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "ACDC - Thunderstruck vs David Guetta, Benny Benassi - Satisfaction",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Maak de zin af",
+    "URL": "https://www.youtube.com/watch?v=eg8YHJmmLfc&list=RDeg8YHJmmLfc&start_radio=1",
+    "StartTijd": 0,
+    "EindTijd": 40,
+    "Vraag": "",
+    "Antwoord": "Liever te dik in de kist. Dan weer een feestje gemist.",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=MA0-ZBodwbw&list=RDMA0-ZBodwbw&start_radio=1",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "AC/DC vs. Bruno Mars - Back in Black vs Uptown Funck",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=82LCKBdjywQ&list=RDtWbemAoPlWw&index=3",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "PSY vs Ghostbusters - Gangnam style vs Ghost Busters",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=J4LD20WHkXM&list=RDtWbemAoPlWw&index=2",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Bomfunk MC's - Freestyler (Instrumental) Eminem - Kamikaze (Acapella) Eminem - Criminal (Acapella) Busta Rhymes - Break Ya Neck (Acapella) Eminem ft. Juice WRLD - Godzilla (Acapella) Mortal Kombat (Announcer)",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Mashup",
+    "URL": "https://www.youtube.com/watch?v=YHQ-9RBzSZU&list=RDtWbemAoPlWw&index=4",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Rasputin - Boney M. Hotel Room Service - Pitbull",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the songs",
+    "URL": "https://www.youtube.com/watch?v=A_SaCZbENZk&list=RDtWbemAoPlWw&index=6",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Los Del Río - Macarena Disturbed - Stupify",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the songs",
+    "URL": "https://www.youtube.com/watch?v=SE0bQ7WQF9c&list=RDtWbemAoPlWw&index=7",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Michael Jackson's \"Billie Jean\" and Eric Clapton's \"Cocaine",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the songs",
+    "URL": "https://www.youtube.com/watch?v=7S3BlhTTCBc&list=RDtWbemAoPlWw&index=8",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Aerosmith \"Walk this way\" Vs Queen \"Another one bites the dust\" & Prince \"Kiss",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the songs",
+    "URL": "https://www.youtube.com/watch?v=n-T0GjrMSs8&list=RDtWbemAoPlWw&index=9",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "Rage Against The Machine's 'killing in the Name' & Creedence Clearwater Revival's 'Bad Moon Rising'",
+    "@dropdown": ""
+  },
+  {
+    "Type": "Guess the songs",
+    "URL": "https://www.youtube.com/watch?v=Ezt-NlP2-uI&list=RDtWbemAoPlWw&index=15",
+    "StartTijd": "",
+    "EindTijd": "",
+    "Vraag": "What songs do you hear?",
+    "Antwoord": "The Bee Gees - Stayin' Alive x 50 cent - In Da Club",
+    "@dropdown": ""
+  }
 ]
+
 
 
 
