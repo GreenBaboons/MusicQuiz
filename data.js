ef5e8ea5a9a39e85b9587f684295182933a03cfc
GreenBaboon <65962651+GreenBaboons@users.noreply.github.com>
2025-12-31 10:30:03 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,51 +1,6 @@
 // data.js
 const quizData = [
     {
-        "Type": "Intro",
-        "URL": "https://www.youtube.com/watch?v=gJnNW0RWpO0&list=RDgJnNW0RWpO0&start_radio=1",
-        "StartTijd": 15,
-        "EindTijd": 90,
-        "Vraag": "Welke band?",
-        "Antwoord": "Queen",
-        "Info": "Uit 1975"
-    },
-    {
-        "Type": "Mashup",
-        "URL": "https://www.youtube.com/watch?v=dh-RUBbGZZA",
-        "StartTijd": 0,
-        "EindTijd": 90,
-        "Vraag": "Nummers & Artiesten",
-        "Antwoord": "Adele - Set fire to the rain & Linkin Park - In the end",
-        "Info": ""
-    },
-    {
-        "Type": "Maak de zin af",
-        "URL": "https://www.youtube.com/watch?v=eg8YHJmmLfc&list=RDeg8YHJmmLfc&start_radio=1",
-        "StartTijd": 0,
-        "EindTijd": 40,
-        "Vraag": "",
-        "Antwoord": "Liever te dik in de kist. Dan weer een feestje gemist.",
-        "Info": ""
-    },
-    {
-        "Type": "Song & Artist",
-        "URL": "https://www.youtube.com/watch?v=idBsmslkKDs&list=RDidBsmslkKDs&start_radio=1",
-        "StartTijd": 30,
-        "EindTijd": 90,
-        "Vraag": "Who's song is this?",
-        "Antwoord": "Queen - Bohemian Rhapsody",
-        "Info": "Uit 1975"
-    },
-    {
-        "Type": "Maak de zin af",
-        "URL": "https://www.youtube.com/watch?v=eg8YHJmmLfc&list=RDeg8YHJmmLfc&start_radio=1",
-        "StartTijd": 0,
-        "EindTijd": 40,
-        "Vraag": "",
-        "Antwoord": "Liever te dik in de kist. Dan weer een feestje gemist.",
-        "Info": ""
-    },
-    {
         "Type": "Mashup",
         "URL": "https://www.youtube.com/watch?v=_AGYdfip4x0&list=PLG-S-nXFghGhE7Mg7X3_uXSQeAZwFfoZR",
         "StartTijd": "",
@@ -154,5 +109,6 @@ const quizData = [
         "Info": ""
     }
 ]
+
 
 
